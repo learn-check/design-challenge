f34f28b39d8f6a9c63465edc68dc93e6a3e5b5e6
artofghost <71373225+artofghost@users.noreply.github.com>
2020-11-10 13:09:26 +0100

    form controle

--- Media/Website/JS/nav.js
+++ Media/Website/JS/nav.js
@@ -6,18 +6,17 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
   // dit sluit de sidenav
-  function modal() {
-    modal()
-  }
+ 
 
   function areUSure(){
     var r = confirm("weet je het zeker dat je deze reis wilt?");
-    var home = document.getElementsByName("../home.html")
+    
     if (r == true){
       document.getElementById("button").action;
     }
     else{
       alert("dat is jamer tot de volgende keer")
+      return false;
     }
   }
   // dit is een jav functie die ik heb geschreven zodat  de klant er zeker van is dat hij de goede reis heeft gekozen.
