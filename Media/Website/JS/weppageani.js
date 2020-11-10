353f9da4a072cb75f4828cae1e916360a5756bfa
artofghost <71373225+artofghost@users.noreply.github.com>
2020-11-10 10:52:09 +0100

    udates voor data en info pages

--- Media/Website/JS/weppageani.js
+++ Media/Website/JS/weppageani.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
-
+// hier maak ik de const timeline aan. en zet ik de default aimatie naa "power2.out"
 
 tl.to(".slider", { y: "-100%", duration: 1});
 // dit is ervoor zodat de roze slide van onder naar boven gaat.
