a15ed2ef85a5fe050e8323705c7c78e0e1387342
artofghost <71373225+artofghost@users.noreply.github.com>
2020-11-10 12:49:21 +0100

    js update

--- Media/Website/JS/nav.js
+++ Media/Website/JS/nav.js
@@ -1,10 +1,23 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
   }
+  // dit opent de side nav
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
-  
+  // dit sluit de sidenav
   function modal() {
     modal()
   }
+
+  function areUSure(){
+    var r = confirm("weet je het zeker dat je deze reis wilt?");
+    var home = document.getElementsByName("../home.html")
+    if (r == true){
+      document.getElementById("button").action;
+    }
+    else{
+      alert("dat is jamer tot de volgende keer")
+    }
+  }
+  // dit is een jav functie die ik heb geschreven zodat  de klant er zeker van is dat hij de goede reis heeft gekozen.
