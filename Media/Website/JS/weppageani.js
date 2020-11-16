b93dc44ea3a829fd2069887bb1eca6b7cf100b21
artofghost <71373225+artofghost@users.noreply.github.com>
2020-11-16 14:00:50 +0100

    video

--- Media/Website/JS/weppageani.js
+++ Media/Website/JS/weppageani.js
@@ -1,3 +1,4 @@
+
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 // hier maak ik de const timeline aan. en zet ik de default aimatie naa "power2.out"
 
@@ -5,6 +6,7 @@ tl.to(".slider", { y: "-100%", duration: 1});
 // dit is ervoor zodat de roze slide van onder naar boven gaat.
 tl.to(".intro", { y: "-100%", duration: 0.5 }, "-=1");
 // dit is ervoor om de pagina weer tevoorschijn te krijgen.
+
 
 
 
